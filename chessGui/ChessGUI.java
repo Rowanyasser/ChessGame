5b80722a794b9f82428372a3e72d4a81bf00bccc
Rowanyasser <151407136+Rowanyasser@users.noreply.github.com>
2025-04-26 00:39:45 +0300

    Add files via upload

---
+++ chessGui/ChessGUI.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chessGui;
+
+/**
+ *
+ * @author Yomna
+ */
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import javax.swing.*;
+
+public class ChessGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FrontBoard F=new FrontBoard();
+        F.setVisible(true);
+    }
+    
+}
